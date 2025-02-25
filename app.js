48d91ef2e9ac957276d941450460352c7fdaa2be
Expep <89018966+Expep@users.noreply.github.com>
2025-02-25 16:44:22 +0400

    Finished

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 let tip = document.getElementById('tipPercent');
 let bill = document.getElementById('bill');
-
+let total = document.getElementById('total')
 
 
 const Calculatetip = () => {
-    let tipAmount  =  (bill.value/ tip.value)/100      
-    return tipAmount
+    let tipAmount  =  bill.value * (tip.value/100)  
+    return total.innerHTML=  Math.round( +bill.value + tipAmount) + ('.00')
 }   
